51637ca0afeef7bcfef3fc633933a79a14b24afc
rafdhiyaulhaq <m.rafidhiyaulhaq@gmail.com>
2024-11-12 19:25:07 +0700

    delete module

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'VirtualLabFisika',
+  location: 'us-central1'
+};
+
